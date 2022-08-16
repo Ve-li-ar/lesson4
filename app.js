b19651b2805c8bbf6970665bba182ee45b96341c
Lina Veliar <veliar.helly@gmail.com>
2022-08-16 20:15:49 +0300

    add lesson4

---
+++ app.js
@@ -0,0 +1,39 @@
+const board = document.querySelector("#board");
+const SQUARES_NUMBER = 1450;
+const colors = [
+  "#412034",
+  "#722950",
+  "#7F4C63",
+  "#7D8381",
+  "#D7C77C",
+  "#45423F",
+];
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", setColor);
+
+  square.addEventListener("mouseleave", removeColor);
+
+  board.append(square);
+}
+
+function setColor(event) {
+    const element = event.target
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(event) {
+    const element = event.target
+
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
